4504fb239d00c5293704f578ae1869ab35d646d0
avsgsta <supratmanjaja9@gmail.com>
2024-01-27 23:22:53 +0900

    Initial commit

---
+++ app/src/main/java/com/azhar/spks/activities/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.azhar.spks.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.azhar.spks.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(decorView.SYSTEM_UI_FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 3000);
+
+        ProgressBar LoadingProgressBar = findViewById(R.id.progressBar);
+        LoadingProgressBar.setIndeterminate(true);
+    }
+}
